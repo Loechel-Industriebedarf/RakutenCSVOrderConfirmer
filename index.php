4097e9407f53e07cdd595fb202354444b7201230
ADarkHero <maik.riedlsperger@gmail.com>
2020-01-29 09:19:18 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php	
+	$file = fopen('tracking.csv', 'r');
+	$baseurl = 'http://webservice.rakuten.de/merchants/orders/setOrderShipped?key=INPUT_API_KEY_HERE';
+	while (($line = fgetcsv($file, 0, ";")) !== FALSE) {
+		$apiurl = $baseurl . '&order_no=' . $line[0] . '&carrier=' . $line[1] . '&tracking_number=' . $line[2];
+	   
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $apiurl); //Add url to curl	
+		$response = curl_exec($curl); //Call url via curl
+		
+		
+		var_dump($response); //Show response
+		echo "<br>";
+	}
+	
+	fclose($file);
+?>
