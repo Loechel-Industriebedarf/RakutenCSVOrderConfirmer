b4d4d977082235ac87d94eeb2e7dc8bf6684e13e
ADarkHero <maik.riedlsperger@gmail.com>
2020-05-18 15:53:03 +0200

    Added an echo for order-number, carrier and tracking number

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php	
-	$file = fopen('tracking.csv', 'r');
+	$file = fopen('rakutentracking.csv', 'r');
 	$baseurl = 'http://webservice.rakuten.de/merchants/orders/setOrderShipped?key=INPUT_API_KEY_HERE';
 	while (($line = fgetcsv($file, 0, ";")) !== FALSE) {
 		$apiurl = $baseurl . '&order_no=' . $line[0] . '&carrier=' . $line[1] . '&tracking_number=' . $line[2];
@@ -7,9 +7,9 @@
 		$curl = curl_init();
 		curl_setopt($curl, CURLOPT_URL, $apiurl); //Add url to curl	
 		$response = curl_exec($curl); //Call url via curl
-		
-		
+			
 		var_dump($response); //Show response
+		echo ' | ' . $line[0] . ' ' . $line[1] . ' ' . $line[2];
 		echo "<br>";
 	}
 	
